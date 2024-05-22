47b2aba23971bf57349a46a8197141a5c3ff474b
Zoey Joiz De Las Alas <arrazoe@gmail.com>
2024-05-22 11:42:29 -0700

    Added cat jump
    Added jump to void update and started mouse camera follow

--- FinalProjectP3JZL/Assets/Scripts/CatController.cs
+++ FinalProjectP3JZL/Assets/Scripts/CatController.cs
@@ -7,23 +7,47 @@ public class CatController : MonoBehaviour
     public float horzInput;
     public float vertInput;
     public float speed;
-    public float turnSpeed;
-    Rigidbody rb;
+    
+    public float jumpForce;
+    public bool isOnGround;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Side movement
         horzInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horzInput * Time.deltaTime * speed);
 
+        //Forwrd movement
         vertInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * vertInput * Time.deltaTime * speed);
 
+        //If SPACE, jump and can't double 
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+
+        //Have cat turn in cursor direction
+
+
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+       isOnGround = true;
+    }
+
+
+
+
+
 }
